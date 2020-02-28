c5ad810ffdeb0a424a15e1703c6cd6f1dc3e0d07
Jan Janßen <janssen@mpie.de>
2020-02-28 18:33:16 +0100

    Add more functions and an example

--- pylammpsmpi/lammps.py
+++ pylammpsmpi/lammps.py
@@ -2,6 +2,7 @@
 # Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
+import os
 import pickle
 import subprocess
 
@@ -21,7 +22,7 @@ __date__ = "Feb 28, 2020"
 class LammpsLibrary(object):
     def __init__(self, cores=1, working_directory="."):
         executable = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "lmpmpi.py"
+            os.path.dirname(os.path.abspath(__file__)), "mpi", "lmpmpi.py"
         )
         self._process = subprocess.Popen(
             ["mpiexec", "-n", str(cores), "python", executable],
@@ -52,14 +53,181 @@ class LammpsLibrary(object):
         output = pickle.load(self._process.stdout)
         return output
 
-    def command(self, command):
+    def version(self):
+        self._send(command="get_version", data=[])
+        return self._receive()
+
+    def file(self, *args):
+        self._send(command="get_file", data=list(args))
+
+    def commands_list(self, *args):
+        self._send(command="commands_list", data=list(args))
+
+    def commands_string(self, *args):
+        self._send(command="commands_string", data=list(args))
+
+    def extract_setting(self, *args):
+        self._send(command="extract_setting", data=list(args))
+        return self._receive()
+
+    def extract_global(self, *args):
+        self._send(command="extract_global", data=list(args))
+        return self._receive()
+
+    def extract_box(self):
+        self._send(command="extract_box", data=[])
+        return self._receive()
+
+    def extract_atom(self, *args):
+        self._send(command="extract_atom", data=list(args))
+        return self._receive()
+
+    def extract_fix(self, *args):
+        self._send(command="extract_fix", data=list(args))
+        return self._receive()
+
+    def extract_variable(self, *args):
+        self._send(command="extract_variable", data=list(args))
+        return self._receive()
+
+    def get_natoms(self):
+        self._send(command="get_natoms", data=[])
+        return self._receive()
+
+    def set_variable(self, *args):
+        self._send(command="set_variable", data=list(args))
+        return self._receive()
+
+    def reset_box(self, *args):
+        self._send(command="reset_box", data=list(args))
+
+    def gather_atoms_concat(self, *args):
+        self._send(command="gather_atoms_concat", data=list(args))
+        return self._receive()
+
+    def gather_atoms_subset(self, *args):
+        self._send(command="gather_atoms_subset", data=list(args))
+        return self._receive()
+
+    def scatter_atoms_subset(self, *args):
+        self._send(command="scatter_atoms_subset", data=list(args))
+
+    def create_atoms(self, *args):
+        self._send(command="create_atoms", data=list(args))
+
+    @property
+    def has_exceptions(self):
+        """ Return whether the LAMMPS shared library was compiled with C++ exceptions handling enabled """
+        self._send(command="has_exceptions", data=[])
+        return self._receive()
+
+    @property
+    def has_gzip_support(self):
+        self._send(command="has_gzip_support", data=[])
+        return self._receive()
+
+    @property
+    def has_png_support(self):
+        self._send(command="has_png_support", data=[])
+        return self._receive()
+
+    @property
+    def has_jpeg_support(self):
+        self._send(command="has_jpeg_support", data=[])
+        return self._receive()
+
+    @property
+    def has_ffmpeg_support(self):
+        self._send(command="has_ffmpeg_support", data=[])
+        return self._receive()
+
+    @property
+    def installed_packages(self):
+        self._send(command="get_installed_packages", data=[])
+        return self._receive()
+
+    def set_fix_external_callback(self, *args):
+        self._send(command="set_fix_external_callback", data=list(args))
+
+    def get_neighlist(self, *args):
+        """Returns an instance of :class:`NeighList` which wraps access to the neighbor list with the given index
+        :param idx: index of neighbor list
+        :type  idx: int
+        :return: an instance of :class:`NeighList` wrapping access to neighbor list data
+        :rtype:  NeighList
+        """
+        self._send(command="get_neighlist", data=list(args))
+        return self._receive()
+
+    def find_pair_neighlist(self, *args):
+        """Find neighbor list index of pair style neighbor list
+        Try finding pair instance that matches style. If exact is set, the pair must
+        match style exactly. If exact is 0, style must only be contained. If pair is
+        of style pair/hybrid, style is instead matched the nsub-th hybrid sub-style.
+        Once the pair instance has been identified, multiple neighbor list requests
+        may be found. Every neighbor list is uniquely identified by its request
+        index. Thus, providing this request index ensures that the correct neighbor
+        list index is returned.
+        :param style: name of pair style that should be searched for
+        :type  style: string
+        :param exact: controls whether style should match exactly or only must be contained in pair style name, defaults to True
+        :type  exact: bool, optional
+        :param nsub:  match nsub-th hybrid sub-style, defaults to 0
+        :type  nsub:  int, optional
+        :param request:   index of neighbor list request, in case there are more than one, defaults to 0
+        :type  request:   int, optional
+        :return: neighbor list index if found, otherwise -1
+        :rtype:  int
+         """
+        self._send(command="find_pair_neighlist", data=list(args))
+        return self._receive()
+
+    def find_fix_neighlist(self, *args):
+        """Find neighbor list index of fix neighbor list
+        :param fixid: name of fix
+        :type  fixid: string
+        :param request:   index of neighbor list request, in case there are more than one, defaults to 0
+        :type  request:   int, optional
+        :return: neighbor list index if found, otherwise -1
+        :rtype:  int
+         """
+        self._send(command="find_fix_neighlist", data=list(args))
+        return self._receive()
+
+    def find_compute_neighlist(self, *args):
+        """Find neighbor list index of compute neighbor list
+        :param computeid: name of compute
+        :type  computeid: string
+        :param request:   index of neighbor list request, in case there are more than one, defaults to 0
+        :type  request:   int, optional
+        :return: neighbor list index if found, otherwise -1
+        :rtype:  int
+         """
+        self._send(command="find_compute_neighlist", data=list(args))
+        return self._receive()
+
+    def get_neighlist_size(self, *args):
+        """Return the number of elements in neighbor list with the given index
+        :param idx: neighbor list index
+        :type  idx: int
+        :return: number of elements in neighbor list with index idx
+        :rtype:  int
+         """
+        self._send(command="get_neighlist_size", data=list(args))
+        return self._receive()
+
+    def get_neighlist_element_neighbors(self, *args):
+        self._send(command="get_neighlist_element_neighbors", data=list(args))
+        return self._receive()
+
+    def command(self, cmd):
         """
         Send a command to the lammps library
 
         Args:
-            command (str):
+            cmd (str):
         """
-        self._send(command="command", data=command)
+        self._send(command="command", data=cmd)
 
     def gather_atoms(self, *args):
         """
---
+++ pylammpsmpi/mpi/lmpmpi.py
@@ -0,0 +1,226 @@
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+
+from ctypes import c_double, c_int
+from mpi4py import MPI
+import numpy as np
+import pickle
+import sys
+from lammps import lammps
+
+__author__ = "Jan Janssen"
+__copyright__ = (
+    "Copyright 2020, Max-Planck-Institut für Eisenforschung GmbH - "
+    "Computational Materials Design (CM) Department"
+)
+__version__ = "1.0"
+__maintainer__ = "Jan Janssen"
+__email__ = "janssen@mpie.de"
+__status__ = "production"
+__date__ = "Feb 28, 2020"
+
+
+# Lammps executable
+job = lammps(cmdargs=["-screen", "none"])
+
+
+def extract_compute(funct_args):
+    if MPI.COMM_WORLD.rank == 0:
+        return np.array(job.extract_compute(*funct_args))
+
+
+def get_version(funct_args):
+    if MPI.COMM_WORLD.rank == 0:
+        return job.version()
+
+
+def get_file(funct_args):
+    job.file(*funct_args)
+
+
+def commands_list(funct_args):
+    job.commands_list(*funct_args)
+
+
+def commands_string(funct_args):
+    job.commands_string(*funct_args)
+
+
+def extract_setting(funct_args):
+    if MPI.COMM_WORLD.rank == 0:
+        return job.extract_setting(*funct_args)
+
+
+def extract_global(funct_args):
+    if MPI.COMM_WORLD.rank == 0:
+        return job.extract_global(*funct_args)
+
+
+def extract_box(funct_args):
+    if MPI.COMM_WORLD.rank == 0:
+        return job.extract_box(*funct_args)
+
+
+def extract_atom(funct_args):
+    if MPI.COMM_WORLD.rank == 0:
+        return job.extract_atom(*funct_args)
+
+
+def extract_fix(funct_args):
+    if MPI.COMM_WORLD.rank == 0:
+        return job.extract_fix(*funct_args)
+
+
+def extract_variable(funct_args):
+    if MPI.COMM_WORLD.rank == 0:
+        return job.extract_variable(*funct_args)
+
+
+def get_natoms(funct_args):
+    if MPI.COMM_WORLD.rank == 0:
+        return job.get_natoms()
+
+
+def set_variable(funct_args):
+    return job.set_variable(*funct_args)
+
+
+def reset_box(funct_args):
+    job.reset_box()
+
+
+def gather_atoms_concat(funct_args):
+    return np.array(job.gather_atoms_concat(*funct_args))
+
+
+def gather_atoms_subset(funct_args):
+    return np.array(job.gather_atoms_subset(*funct_args))
+
+
+def scatter_atoms_subset(funct_args):
+    job.scatter_atoms_subset(*funct_args)
+
+
+def create_atoms(funct_args):
+    job.create_atoms(*funct_args)
+
+
+def has_exceptions(funct_args):
+    return job.has_exceptions
+
+
+def has_gzip_support(funct_args):
+    return job.has_gzip_support
+
+
+def has_png_support(funct_args):
+    return job.has_png_support
+
+
+def has_jpeg_support(funct_args):
+    return job.has_jpeg_support
+
+
+def has_ffmpeg_support(funct_args):
+    return job.has_ffmpeg_support
+
+
+def installed_packages(funct_args):
+    return job.installed_packages
+
+
+def set_fix_external_callback(funct_args):
+    job.set_fix_external_callback(*funct_args)
+
+
+def get_neighlist(funct_args):
+    if MPI.COMM_WORLD.rank == 0:
+        return job.get_neighlist(*funct_args)
+
+
+def find_pair_neighlist(funct_args):
+    if MPI.COMM_WORLD.rank == 0:
+        return job.find_pair_neighlist(*funct_args)
+
+
+def find_fix_neighlist(funct_args):
+    if MPI.COMM_WORLD.rank == 0:
+        return job.find_fix_neighlist(*funct_args)
+
+
+def find_compute_neighlist(funct_args):
+    if MPI.COMM_WORLD.rank == 0:
+        return job.find_compute_neighlist(*funct_args)
+
+
+def get_neighlist_size(funct_args):
+    if MPI.COMM_WORLD.rank == 0:
+        return job.get_neighlist_size(*funct_args)
+
+
+def get_neighlist_element_neighbors(funct_args):
+    if MPI.COMM_WORLD.rank == 0:
+        return job.get_neighlist_element_neighbors(*funct_args)
+
+
+def get_thermo(funct_args):
+    if MPI.COMM_WORLD.rank == 0:
+        return np.array(job.get_thermo(*funct_args))
+
+
+def scatter_atoms(funct_args):
+    py_vector = funct_args[3]
+    if issubclass(type(py_vector[0]), np.integer):
+        c_vector = (len(py_vector) * c_int)(*py_vector)
+    else:
+        c_vector = (len(py_vector) * c_double)(*py_vector)
+    job.scatter_atoms(funct_args[0], funct_args[1], funct_args[2], c_vector)
+
+
+def command(funct_args):
+    job.command(funct_args)
+
+
+def gather_atoms(funct_args):
+    return np.array(job.gather_atoms(*funct_args))
+
+
+def select_cmd(argument):
+    """
+    Select a lammps command
+
+    Args:
+        argument (str): [close, extract_compute, get_thermo, scatter_atoms, command, gather_atoms]
+
+    Returns:
+        function: the selected function
+    """
+    switcher = {
+        f.__name__: f
+        for f in [extract_compute, get_version, get_file, commands_list, commands_string,
+                  extract_setting, extract_global, extract_box, extract_atom, extract_fix, extract_variable,
+                  get_natoms, set_variable, reset_box, gather_atoms_concat, gather_atoms_subset,
+                  scatter_atoms_subset, create_atoms, has_exceptions, has_gzip_support, has_png_support,
+                  has_jpeg_support, has_ffmpeg_support, installed_packages, set_fix_external_callback,
+                  get_neighlist, find_pair_neighlist, find_fix_neighlist, find_compute_neighlist, get_neighlist_size,
+                  get_neighlist_element_neighbors, get_thermo, scatter_atoms, command, gather_atoms]
+    }
+    return switcher.get(argument)
+
+
+if __name__ == "__main__":
+    while True:
+        if MPI.COMM_WORLD.rank == 0:
+            input_dict = pickle.load(sys.stdin.buffer)
+        else:
+            input_dict = None
+        input_dict = MPI.COMM_WORLD.bcast(input_dict, root=0)
+        if input_dict["c"] == "close":
+            job.close()
+            break
+        output = select_cmd(input_dict["c"])(input_dict["d"])
+        if MPI.COMM_WORLD.rank == 0 and output is not None:
+            pickle.dump(output, sys.stdout.buffer)
+            sys.stdout.flush()
+
