f08a45a0afb441096f9b8b6deffea2e4e03b0886
pyiron-runner <pyiron@mpie.de>
2023-02-21 16:07:50 +0000

    Format black

--- pylammpsmpi/mpi/lmpmpi.py
+++ pylammpsmpi/mpi/lmpmpi.py
@@ -57,12 +57,12 @@ def extract_compute(funct_args):
     val_gather = MPI.COMM_WORLD.gather(val, root=0)
     if MPI.COMM_WORLD.rank == 0:
         # val_gather.shape [number of cores, atoms on specific core]
-        # the number of atoms on specific cores can vary 
+        # the number of atoms on specific cores can vary
         val = []
         for vl in val_gather:
             for v in vl:
                 val.append(v)
-        
+
         length = funct_args[3]
         width = funct_args[4]
 
