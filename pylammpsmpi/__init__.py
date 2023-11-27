acab6041eaf448a8d51b01045fa7214318d6b3bf
Jan Janssen <janssen@mpie.de>
2023-11-27 09:36:52 +0100

    Do not depend on pympipool during build

--- pylammpsmpi/__init__.py
+++ pylammpsmpi/__init__.py
@@ -1,10 +1,9 @@
-from pylammpsmpi.wrapper.extended import LammpsLibrary
-from pylammpsmpi.wrapper.concurrent import LammpsConcurrent
-from pylammpsmpi.wrapper.base import LammpsBase
-
 from ._version import get_versions
 
 try:
+    from pylammpsmpi.wrapper.extended import LammpsLibrary
+    from pylammpsmpi.wrapper.concurrent import LammpsConcurrent
+    from pylammpsmpi.wrapper.base import LammpsBase
     from pylammpsmpi.wrapper.ase import LammpsASELibrary
 except ImportError:
     pass
