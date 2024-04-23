5b691fe7fc8dea24d2a31a0d0c934b5eb51c4f1f
Jan Janssen <janssen@mpie.de>
2024-04-23 08:47:07 -0500

    extend test

--- tests/test_ase_interface.py
+++ tests/test_ase_interface.py
@@ -7,9 +7,8 @@ from pylammpsmpi import LammpsASELibrary, LammpsLibrary
 
 
 class TestLammpsASELibrary(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.lmp = LammpsASELibrary(
+    def test_static(self):
+        lmp = LammpsASELibrary(
             working_directory=None,
             cores=1,
             comm=None,
@@ -18,17 +17,11 @@ class TestLammpsASELibrary(unittest.TestCase):
             library=LammpsLibrary(cores=2, mode='local'),
             diable_log_file=True,
         )
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.lmp.close()
-
-    def test_static(self):
         structure = bulk("Al", cubic=True).repeat([2, 2, 2])
-        self.lmp.interactive_lib_command(command="units	lj")
-        self.lmp.interactive_lib_command(command="atom_style atomic")
-        self.lmp.interactive_lib_command(command="atom_modify map array")
-        self.lmp.interactive_structure_setter(
+        lmp.interactive_lib_command(command="units	lj")
+        lmp.interactive_lib_command(command="atom_style atomic")
+        lmp.interactive_lib_command(command="atom_modify map array")
+        lmp.interactive_structure_setter(
             structure=structure,
             units="lj",
             dimension=3,
@@ -37,10 +30,50 @@ class TestLammpsASELibrary(unittest.TestCase):
             el_eam_lst=["Al"],
             calc_md=False,
         )
-        self.lmp.interactive_lib_command("pair_style lj/cut 6.0")
-        self.lmp.interactive_lib_command("pair_coeff 1 1 1.0 1.0 4.04")
-        self.lmp.interactive_lib_command("run 0")
-        self.assertTrue(np.all(np.isclose(self.lmp.interactive_cells_getter(), structure.cell.array)))
-        self.assertTrue(np.isclose(self.lmp.interactive_energy_pot_getter(), -0.04342932384411341))
-        self.assertTrue(np.isclose(self.lmp.interactive_energy_tot_getter(), -0.04342932384411341))
-        self.assertTrue(np.isclose(np.sum(self.lmp.interactive_forces_getter()), 0.0))
+        lmp.interactive_lib_command("pair_style lj/cut 6.0")
+        lmp.interactive_lib_command("pair_coeff 1 1 1.0 1.0 4.04")
+        lmp.interactive_lib_command("run 0")
+        self.assertTrue(np.all(np.isclose(lmp.interactive_cells_getter(), structure.cell.array)))
+        self.assertTrue(np.isclose(lmp.interactive_energy_pot_getter(), -0.04342932384411341))
+        self.assertTrue(np.isclose(lmp.interactive_energy_tot_getter(), -0.04342932384411341))
+        self.assertTrue(np.isclose(np.sum(lmp.interactive_forces_getter()), 0.0))
+        self.assertTrue(np.isclose(lmp.interactive_volume_getter(), 531.4409999999999))
+        self.assertTrue(np.all(lmp.interactive_indices_getter() == [1] * len(structure)))
+        self.assertEqual(lmp.interactive_steps_getter(), 0)
+        self.assertEqual(lmp.interactive_temperatures_getter(), 0)
+        lmp.close()
+
+    def test_static_with_statement(self):
+        structure = bulk("Al", cubic=True).repeat([2, 2, 2])
+        with LammpsASELibrary(
+            working_directory=None,
+            cores=1,
+            comm=None,
+            logger=None,
+            log_file=None,
+            library=LammpsLibrary(cores=2, mode='local'),
+            diable_log_file=True,
+        ) as lmp:
+            lmp.interactive_lib_command(command="units	lj")
+            lmp.interactive_lib_command(command="atom_style atomic")
+            lmp.interactive_lib_command(command="atom_modify map array")
+            lmp.interactive_structure_setter(
+                structure=structure,
+                units="lj",
+                dimension=3,
+                boundary=" ".join(["p" if coord else "f" for coord in structure.pbc]),
+                atom_style="atomic",
+                el_eam_lst=["Al"],
+                calc_md=False,
+            )
+            lmp.interactive_lib_command("pair_style lj/cut 6.0")
+            lmp.interactive_lib_command("pair_coeff 1 1 1.0 1.0 4.04")
+            lmp.interactive_lib_command("run 0")
+            self.assertTrue(np.all(np.isclose(lmp.interactive_cells_getter(), structure.cell.array)))
+            self.assertTrue(np.isclose(lmp.interactive_energy_pot_getter(), -0.04342932384411341))
+            self.assertTrue(np.isclose(lmp.interactive_energy_tot_getter(), -0.04342932384411341))
+            self.assertTrue(np.isclose(np.sum(lmp.interactive_forces_getter()), 0.0))
+            self.assertTrue(np.isclose(lmp.interactive_volume_getter(), 531.4409999999999))
+            self.assertTrue(np.all(lmp.interactive_indices_getter() == [1] * len(structure)))
+            self.assertEqual(lmp.interactive_steps_getter(), 0)
+            self.assertEqual(lmp.interactive_temperatures_getter(), 0)
