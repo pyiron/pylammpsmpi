d380007a437ce1fc0dcb3613875d51b7fdb7c7b7
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-05-14 14:07:42 -0500

    Update __init__.py

--- pylammpsmpi/__init__.py
+++ pylammpsmpi/__init__.py
@@ -4,11 +4,13 @@ from pylammpsmpi.wrapper.extended import LammpsLibrary
 from pylammpsmpi.wrapper.concurrent import LammpsConcurrent
 from pylammpsmpi.wrapper.base import LammpsBase
 
+
+__all__ = [LammpsLibrary, LammpsConcurrent, LammpsBase]
+__version__ = get_versions()["version"]
+
+
 try:
     from pylammpsmpi.wrapper.ase import LammpsASELibrary
+    __all__ += [LammpsASELibrary]
 except ImportError:
     pass
-
-
-__version__ = get_versions()["version"]
-__all__ = [LammpsLibrary, LammpsConcurrent, LammpsBase, LammpsASELibrary]
