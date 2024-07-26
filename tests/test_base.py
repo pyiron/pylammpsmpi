65290e9c7ed553542263d8a37670b0d5d17e8bc4
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-07-26 07:16:09 +0200

    Update test_base.py

--- tests/test_base.py
+++ tests/test_base.py
@@ -109,7 +109,7 @@ class TestLammpsBase(unittest.TestCase):
         self.assertTrue(os.path.isfile(self.citation_file))
 
     def test_version(self):
-        self.assertTrue(self.lmp.version in [20220623, 20230802, 20231121, 20240207])
+        self.assertTrue(self.lmp.version in [20220623, 20230802, 20231121, 20240207, 20240627])
 
     def test_extract_global(self):
         self.assertEqual(
