099c847671e7247e3790fa608b1fc1857d32f19c
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-11-20 20:03:52 +0100

    Update concurrent.py

--- pylammpsmpi/wrapper/concurrent.py
+++ pylammpsmpi/wrapper/concurrent.py
@@ -11,7 +11,7 @@ from typing import Any, List, Optional
 from executorlib.standalone.interactive.communication import interface_bootup
 from executorlib.standalone.interactive.spawner import MpiExecSpawner
 from executorlib.standalone.thread import RaisingThread
-from exeuctorlib.standalone.queue import cancel_items_in_queue
+from executorlib.standalone.queue import cancel_items_in_queue
 
 __author__ = "Sarath Menon, Jan Janssen"
 __copyright__ = (
