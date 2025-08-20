5f0b07055291c6d826f2260b0d7335be7eb29959
pyiron-runner <pyiron@mpie.de>
2025-08-20 16:02:40 +0000

    Format black

--- pylammpsmpi/wrapper/concurrent.py
+++ pylammpsmpi/wrapper/concurrent.py
@@ -414,16 +414,25 @@ class LammpsConcurrent:
         The `caller` arguemnt is a list of inputs to the callback function, including class instances and, importantly, the LammpsLibrary instance itself.
         Since LAMMPS's set_fix_external_callback allows passing a self-reference, we want to keep this feature.
         To transfer the callback function and its associated caller list to the layer containing the actual LAMMPS instance, we serialize them using `dill`.
-        Because the LAMMPS instance is not available at this stage, we insert a placeholder in its position, which will later be replaced with the actual LAMMPS instance during deserialization.  
+        Because the LAMMPS instance is not available at this stage, we insert a placeholder in its position, which will later be replaced with the actual LAMMPS instance during deserialization.
         """
         caller = [
-            "lammps" if hasattr(arg, "__class__") and arg.__class__.__name__ == "LammpsLibrary" else arg
+            (
+                "lammps"
+                if hasattr(arg, "__class__")
+                and arg.__class__.__name__ == "LammpsLibrary"
+                else arg
+            )
             for arg in args[2]
-            ]
-        data = [args[0], dumps(args[1]), [dumps(cl) for cl in caller] if len(args) == 3 else [None]]
+        ]
+        data = [
+            args[0],
+            dumps(args[1]),
+            [dumps(cl) for cl in caller] if len(args) == 3 else [None],
+        ]
         return self._send_and_receive_dict(
             command="set_fix_external_callback", data=data
-            )
+        )
 
     def get_neighlist(self, *args):
         """Returns an instance of :class:`NeighList` which wraps access to the neighbor list with the given index
