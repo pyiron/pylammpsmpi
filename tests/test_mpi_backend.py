c56f2ecd2244b95b45c90aa816cc550e9841da4f
Jan Janssen <janssen@mpie.de>
2025-08-24 10:17:38 +0200

    fix test

--- tests/test_mpi_backend.py
+++ tests/test_mpi_backend.py
@@ -1,11 +1,11 @@
 import unittest
-from lammps import Lammps
+from lammps import lammps
 from pylammpsmpi.mpi.lmpmpi import select_cmd
 
 
 class TestMpiBackend(unittest.TestCase):
     def test_select_cmd(self):
-        lmp = Lammps()
+        lmp = lammps()
         self.assertTrue(
             select_cmd("get_version")(job=lmp, funct_args=[])
             in [20220623, 20230802, 20231121, 20240207, 20240627, 20240829]
