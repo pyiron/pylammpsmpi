745da2d40970f90afffbca3873c8646f466641fa
Jan Janssen <janssen@mpie.de>
2025-09-24 09:30:30 +0200

    move test functions to test

--- tests/test_set_fix_external_callback.py
+++ tests/test_set_fix_external_callback.py
@@ -6,11 +6,37 @@ import unittest
 import numpy as np
 
 from pylammpsmpi import LammpsLibrary
-from pylammpsmpi.helpers.callbacks import (
-    HelperClass,
-    external_callback_without_caller,
-    external_callback_with_caller,
-)
+
+
+def external_callback_without_caller(caller, ntimestep, nlocal, tag, x, f):
+    assert caller is None, "caller is not None"
+    assert isinstance(ntimestep, int), "ntimestep is not int"
+    assert isinstance(nlocal, int), "nlocal is not int"
+    assert isinstance(tag[0], (int, np.integer)), "tag[0] not int-like"
+    assert len(x[0]) == 3, "x[0] must have length 3"
+    assert len(f[0]) == 3, "f[0] must have length 3"
+
+
+def external_callback_with_caller(caller, ntimestep, nlocal, tag, x, f):
+    lmp, helper = caller
+    assert isinstance(helper, HelperClass), "helper is not a HelperClass instance"
+    assert isinstance(helper.token, int), "helper.token is not an int"
+    assert isinstance(ntimestep, int), "ntimestep is not int"
+    assert isinstance(nlocal, int), "nlocal is not int"
+    assert isinstance(tag[0], (int, np.integer)), "tag[0] not int-like"
+    assert len(x[0]) == 3, "x[0] must have length 3"
+    assert len(f[0]) == 3, "f[0] must have length 3"
+    pe = lmp.get_thermo("pe")
+    assert isinstance(float(pe), float), "Potential energy should be float-like"
+
+
+class HelperClass:
+    """
+    Helper object passed through to the external callback.
+    """
+
+    def __init__(self, token: int):
+        self.token = token
 
 
 class TestSetFixExternalCallback(unittest.TestCase):
